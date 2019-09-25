4a21eb6d143fdb0f165865446fe05fa975a211b2
Svetlana <sbatova@bitia.ru>
2019-09-25 10:32:28 +0300

    GKN-167: Add NoticeForm

--- src/Spots/SpotsShow.js
+++ src/Spots/SpotsShow.js
@@ -1172,9 +1172,37 @@ class SpotsShow extends Authorization {
           ? selectedFilters[spotId][sectorId].date
           : DEFAULT_FILTERS.date
       );
+      const {spotId, sectorId} = this.state;
       setSelectedFilterProp(spotId, sectorId, 'date', date);
     }
-    this.reloadRoutes({ date }, null, user, viewMode);
+    this.reloadRoutes({date}, null, user, viewMode);
+    this.setState({viewMode});
+  };
+
+  submitNoticeForm = (msg) => {
+    const { user, selectedFilters } = this.props;
+    const { currentShown, spotId, sectorId } = this.state;
+    Sentry.withScope((scope) => {
+      scope.setExtra('user_id', user.id);
+      scope.setExtra('route_id', currentShown.id);
+      scope.setExtra(
+        'filters',
+        (
+          (selectedFilters && selectedFilters[spotId])
+            ? selectedFilters[spotId][sectorId]
+            : undefined
+        ),
+      );
+      scope.setExtra('url', window.location.href);
+      if (user.login) {
+        scope.setExtra('user_login', user.login);
+      } else if (user.email) {
+        scope.setExtra('user_email', user.email);
+      } else {
+        scope.setExtra('user_phone', user.phone);
+      }
+      Sentry.captureException(msg);
+    });
   };
 
   content = () => {
@@ -1275,6 +1303,7 @@ class SpotsShow extends Authorization {
               ? (currentSector.diagram && currentSector.diagram.url)
               : (sector.diagram && sector.diagram.url)
           }
+          submitNoticeForm={this.submitNoticeForm}
         />
       );
     }
