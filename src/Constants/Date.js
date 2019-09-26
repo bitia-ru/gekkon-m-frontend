9a93eed9735ec6458c45fae691b271e1be8444e9
Svetlana <sbatova@bitia.ru>
2019-09-26 13:38:57 +0300

    GKN-169: Add date formatter to DatePicker

--- src/Constants/Date.js
+++ src/Constants/Date.js
@@ -1,3 +1,12 @@
+import moment from 'moment/moment';
+
 export const COMMENT_DATETIME_FORMAT = 'hh:mm LL';
 export const DATE_FORMAT = 'DD.MM.YYYY';
 export const BACKEND_DATE_FORMAT = 'YYYY-MM-DD';
+
+export const dateToTextFormatter = (date) => {
+  if (moment(date).format(DATE_FORMAT) === moment().format(DATE_FORMAT)) {
+    return 'сегодня';
+  }
+  return moment(date).format(DATE_FORMAT);
+};
