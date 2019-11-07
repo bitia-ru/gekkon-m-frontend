896dda12123c7f8e1c1578d1ad99f28fbc0aec36
ALev <alev@bitia.ru>
2019-11-07 15:53:38 +0300

    release/0.3.0: Fix Facebook link to the final

--- src/Constants/SocialLinks.js
+++ src/Constants/SocialLinks.js
@@ -1,3 +1,3 @@
 export const VK_LINK = 'https://vk.com/roving.climbers';
-export const FACEBOOK_LINK = 'https://t.me/roving.climbers';
+export const FACEBOOK_LINK = 'https://fb.me/roving.climbers';
 export const TWITTER_LINK = 'https://twitter.com/ClimbersRoving';
