d59a02b8d3f81b72b29e661786b4b0c66c045813
ALev <alev@bitia.ru>
2020-02-06 17:36:22 +0300

    Tokyo replaced by Atmo

--- src/Constants/Spots.js
+++ src/Constants/Spots.js
@@ -1,7 +1,7 @@
 import gravityImage from '../../img/spot-card-img/gravity.jpg';
 import limestoneImage from '../../img/spot-card-img/limestone.jpg';
 import bigwallImage from '../../img/spot-card-img/bigwall.jpg';
-import tokyoImage from '../../img/spot-card-img/tokyo.jpg';
+import atmoImage from '../../img/spot-card-img/atmosphere.jpg';
 
 const SPOTS_DATA = [
   {
@@ -23,10 +23,10 @@ const SPOTS_DATA = [
     className: 'climbing-card-m_bigwall',
   },
   {
-    id: 4,
-    name: 'Токио',
-    imgSrc: tokyoImage,
-    className: 'climbing-card-m_tokyo',
+    id: 3,
+    name: 'Атмосфера',
+    imgSrc: atmoImage,
+    className: 'climbing-card-m_atmo',
   },
 ];
 
--- webpack.config.js
+++ webpack.config.js
@@ -11,6 +11,9 @@ module.exports = () => ({
     filename: 'index-[hash].js',
     publicPath: '/',
   },
+  devServer: {
+    historyApiFallback: true,
+  },
   module: {
     rules: [
       {
