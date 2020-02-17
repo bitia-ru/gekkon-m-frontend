9ac0d5fbd12cff9a996be33dc85229ba040e2440
Svetlana Batova <sbatova@bitia.ru>
2020-02-17 19:25:32 +0300

    GKN-208: Add instagram link

--- src/v1/Constants/SocialLinks.js
+++ src/v1/Constants/SocialLinks.js
@@ -1,3 +1,4 @@
+export const INSTA_LINK = 'https://www.instagram.com/rovingclimbers/';
 export const VK_LINK = 'https://vk.com/roving.climbers';
 export const FACEBOOK_LINK = 'https://fb.me/roving.climbers';
 export const TWITTER_LINK = 'https://twitter.com/ClimbersRoving';
