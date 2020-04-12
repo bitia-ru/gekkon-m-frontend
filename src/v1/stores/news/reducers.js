f9eefe71eff054bca0801ac96c90d4a0f651a570
Vertigo <vertigomaill@gmail.com>
2020-04-12 17:03:00 +0300

    GKN-240: Replace R.clone with spread expressions

--- src/v1/stores/news/reducers.js
+++ src/v1/stores/news/reducers.js
@@ -6,18 +6,23 @@ const newsStoreReducer = (
   state = DEFAULT_STORE_FORMAT,
   action,
 ) => {
-  const stateCopy = R.clone(state);
   switch (action.type) {
   case acts.LOAD_NEWS_REQUEST:
-    stateCopy.numOfActiveRequests += 1;
-    return stateCopy;
+    return {
+      ...state,
+      numOfActiveRequests: state.numOfActiveRequests + 1,
+    };
   case acts.LOAD_NEWS_FAILED:
-    stateCopy.numOfActiveRequests -= 1;
-    return stateCopy;
+    return {
+      ...state,
+      numOfActiveRequests: state.numOfActiveRequests - 1,
+    };
   case acts.LOAD_NEWS_SUCCESS:
-    stateCopy.news = R.mergeDeepRight(stateCopy.news, action.news);
-    stateCopy.numOfActiveRequests -= 1;
-    return stateCopy;
+    return {
+      ...state,
+      news: R.mergeDeepRight(state.news, action.news),
+      numOfActiveRequests: state.numOfActiveRequests - 1,
+    };
   default:
     return state;
   }
