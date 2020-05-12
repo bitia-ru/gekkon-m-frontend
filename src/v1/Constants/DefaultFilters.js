6444f211f0f9a8d588e246a10c806aaa737e6597
Svetlana Batova <sbatova@bitia.ru>
2020-05-12 17:54:05 +0300

    GKN-245: Remove duplicates from filters

--- src/v1/Constants/DefaultFilters.js
+++ src/v1/Constants/DefaultFilters.js
@@ -1,19 +1,6 @@
-import * as R from 'ramda';
 import moment from 'moment/moment';
 import RESULT_FILTERS from './ResultFilters';
 import { CATEGORIES } from './Categories';
-
-const result = R.map(e => e.value, RESULT_FILTERS);
-const resultFilters = R.map(
-  e => R.merge(
-    e,
-    {
-      selected: R.find(r => r === e.value, result) !== undefined,
-      text: `${e.text}${R.find(r => r === e.value, result) !== undefined ? ' ✓' : ''}`,
-    },
-  ),
-  RESULT_FILTERS,
-);
 
 export const PERSONAL_DEFAULT = false;
 export const OUTDATED_DEFAULT = false;
@@ -24,33 +11,8 @@ export const DEFAULT_FILTERS = {
   categoryTo: CATEGORIES[CATEGORIES.length - 1],
   period: 0,
   date: moment().format(),
-  result,
+  ...RESULT_FILTERS,
   personal: PERSONAL_DEFAULT,
   outdated: OUTDATED_DEFAULT,
-  filters: R.concat(
-    resultFilters,
-    [
-      {
-        clickable: true,
-        id: 'personal',
-        selected: PERSONAL_DEFAULT,
-        text: `Авторские трассы ${PERSONAL_DEFAULT ? ' ✓' : ''}`,
-        value: 'personal',
-      },
-      {
-        clickable: true,
-        id: 'outdated',
-        selected: OUTDATED_DEFAULT,
-        text: `Скрученные ${OUTDATED_DEFAULT ? ' ✓' : ''}`,
-        value: 'outdated',
-      },
-      {
-        clickable: true,
-        id: 'liked',
-        selected: LIKED_DEFAULT,
-        text: `Понравившиеся мне ${LIKED_DEFAULT ? ' ✓' : ''}`,
-        value: 'liked',
-      },
-    ],
-  ),
+  liked: LIKED_DEFAULT,
 };
--- src/v1/utils/getFilters.js
+++ src/v1/utils/getFilters.js
@@ -1,13 +1,42 @@
+import * as R from 'ramda';
 import store from '../store';
 import { DEFAULT_FILTERS } from '../Constants/DefaultFilters';
+import RESULT_FILTERS from '@/v1/Constants/ResultFilters';
+
+const filtersLookUp = {
+  personal: 'Авторские трассы',
+  outdated: 'Скрученные',
+  liked: 'Понравившиеся мне',
+  flash: 'Флешанул',
+  red_point: 'Пролез',
+  unsuccessful: 'Не пройдена',
+};
+
+export const prepareFilters = (filters) => {
+  return {
+    ...filters,
+    filters: R.map(
+      e => (
+        {
+          clickable: true,
+          id: e,
+          selected: filters[e],
+          text: `${filtersLookUp[e]} ${filters[e] ? ' ✓' : ''}`,
+          value: e,
+        }
+      ),
+      R.concat(R.keys(RESULT_FILTERS), ['personal', 'outdated', 'liked']),
+    ),
+  };
+};
 
 const getFilters = (spotId, sectorId) => {
   const state = store.getState();
   const { selectedFilters } = state;
   if (selectedFilters && selectedFilters[spotId] && selectedFilters[spotId][sectorId]) {
-    return selectedFilters[spotId][sectorId];
+    return prepareFilters(selectedFilters[spotId][sectorId]);
   }
-  return DEFAULT_FILTERS;
+  return prepareFilters(DEFAULT_FILTERS);
 };
 
 export default getFilters;
