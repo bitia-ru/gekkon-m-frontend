e9becba6b3acbe0415e8cc376db68a3b6d3911d3
ALev <alev@bitia.ru>
2020-07-23 22:37:48 +0300

    GKN-259: Rewrite enterWithVk()

--- src/v2/utils/vk.js
+++ src/v2/utils/vk.js
@@ -1,36 +1,51 @@
 import * as R from 'ramda';
-import { CLIENT_ID, REDIRECT_URI } from '@/v1/Constants/Vk';
+import { ApiUrl } from '@/v1/Environ';
+import { CLIENT_ID, REDIRECT_URI_PATH } from '@/v1/Constants/Vk';
 import closeForm from '@/v2/utils/closeForm';
 
+const redirectUri = () => (
+  ApiUrl.match(/^https?:\/\/.*/) ? (
+    `${ApiUrl}${REDIRECT_URI_PATH}`
+  ) : (
+    `${window.location.origin}${ApiUrl}${REDIRECT_URI_PATH}`
+  )
+);
 
-export const enterWithVk = (type, token) => {
-  const params = {
-    client_id: CLIENT_ID,
-    scope: 'email%2Cphotos',
-    redirect_uri: REDIRECT_URI,
-    response_type: 'code',
-    v: '5.74',
-    state: JSON.stringify({
-      method: type,
-      token,
-    }),
-  };
+const params = (type, token) => ({
+  client_id: CLIENT_ID,
+  scope: 'email%2Cphotos',
+  redirect_uri: redirectUri(),
+  response_type: 'code',
+  v: '5.74',
+  state: JSON.stringify({
+    method: type,
+    token,
+  }),
+});
 
-  window.open(
-    `https://oauth.vk.com/authorize?${R.join('&')(R.map(v => `${v[0]}=${v[1]}`)(R.toPairs(params)))}`,
-    'VK',
-    'resizable,scrollbars,status',
-  );
-
-  window.addEventListener('message', afterVkEnter);
-};
+const paramsSerialized = (type, token) => (
+  R.join('&')(
+    R.map(v => `${v[0]}=${v[1]}`)(
+      R.toPairs(params(type, token)),
+    ),
+  )
+);
 
 export const afterVkEnter = (ev) => {
   if (ev.data.result !== 'success') {
     return;
   }
   ev.source.close();
-  //store.dispatch(signIn());
   closeForm();
   window.removeEventListener('message', afterVkEnter);
+};
+
+export const enterWithVk = (type, token) => {
+  window.open(
+    `https://oauth.vk.com/authorize?${paramsSerialized(type, token)}`,
+    'VK',
+    'resizable,scrollbars,status',
+  );
+
+  window.addEventListener('message', afterVkEnter);
 };
