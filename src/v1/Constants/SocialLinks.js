47a5534acf86ac6e5c541bb5dca00e7c4795bd79
ALev <alev@bitia.ru>
2020-08-18 04:44:21 +0300

    GFM-63: Remove creator mode item from user menu

--- src/v1/Constants/SocialLinks.js
+++ src/v1/Constants/SocialLinks.js
@@ -1,4 +1,3 @@
+export const TELEGRAM_LINK = 'https://t.me/crags_ru';
 export const INSTA_LINK = 'https://www.instagram.com/rovingclimbers/';
 export const VK_LINK = 'https://vk.com/roving.climbers';
-export const FACEBOOK_LINK = 'https://fb.me/roving.climbers';
-export const TWITTER_LINK = 'https://twitter.com/ClimbersRoving';
