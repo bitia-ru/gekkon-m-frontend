54500ce165ebb71decbfcbca3efc808e304a2f09
Sergey <vertigomaill@gmail.com>
2020-08-27 12:15:09 +0300

    GFM-66: Move Counter styles into separate file

--- src/v1/components/Counter/Counter.js
+++ src/v1/components/Counter/Counter.js
@@ -1,37 +1,7 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import { css, StyleSheet } from '@/v2/aphrodite';
-
-const styles = StyleSheet.create({
-  counter: {
-    fontSize: '16px',
-    fontFamily: ['GilroyRegular', 'sans-serif'],
-    display: 'flex',
-    alignItems: 'center',
-    height: '100%',
-  },
-  counterText: {
-    height: '100%',
-    textAlign: 'center',
-    lineHeight: '28px',
-  },
-  counterNumContainer: { marginRight: '4px' },
-  counterNumWrapper: {
-    display: 'flex',
-    alignItems: 'center',
-    justifyContent: 'center',
-    color: '#FFFFFF',
-    height: '100%',
-    width: '28px',
-    borderRadius: '50%',
-  },
-  counterNum: { marginTop: '2px' },
-  defaultCounterColor: { color: '#828282' },
-  redpointsTextColor: { color: '#E24D4D' },
-  flashesTextColor: { color: '#000000' },
-  redpointsNumColor: { backgroundColor: '#E24D4D' },
-  flashesNumColor: { backgroundColor: '#000000' },
-});
+import { css } from '@/v2/aphrodite';
+import styles from './styles';
 
 const Counter = ({
   text, number, type,
