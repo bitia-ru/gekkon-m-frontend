90ce8e75503c7350c83d85be854e840d68a5fe46
Svetlana Batova <sbatova@bitia.ru>
2020-09-04 13:32:13 +0300

    GFM-82: Change MainPageHeader.css to aphrodite

--- src/v2/components/MainPageHeader/MainPageHeader.js
+++ src/v2/components/MainPageHeader/MainPageHeader.js
@@ -4,7 +4,6 @@ import { connect } from 'react-redux';
 import { withRouter } from 'react-router-dom';
 import Button from '@/v1/components/Button/Button';
 import { notReady, notExist } from '@/v1/utils';
-import './MainPageHeader.css';
 import { currentUser } from '@/v2/redux/user_session/utils';
 import SocialLinkButton from '@/v1/components/SocialLinkButton/SocialLinkButton';
 import {
@@ -12,6 +11,7 @@ import {
   INSTA_LINK,
   VK_LINK,
 } from '@/v1/Constants/SocialLinks';
+import { css, StyleSheet } from '@/v2/aphrodite';
 
 const bgImage = require('./images/main-page-header.jpg');
 
@@ -45,12 +45,14 @@ class MainPageHeader extends Component {
     return (
       <header
         style={bgImageLoaded ? { backgroundImage: `url(${bgImage})` } : {}}
-        className="first-section-m"
+        className={css(styles.firstSectionM)}
       >
-        <div className="first-section-m__container">
-          <div className="first-section-m__data">
-            <h1 className="first-section-m__header">Не можешь вспомнить свою первую 6С?</h1>
-            <p className="first-section-m__descr">Не пытайся запоминать боль, записывай</p>
+        <div className={css(styles.firstSectionMContainer)}>
+          <div className={css(styles.firstSectionMData)}>
+            <h1 className={css(styles.firstSectionMHeader)}>
+              Не можешь вспомнить свою первую 6С?
+            </h1>
+            <p className={css(styles.firstSectionMDescr)}>Не пытайся запоминать боль, записывай</p>
             {
               (!notReady(user) && notExist(user)) ? (
                 <>
@@ -89,7 +91,7 @@ class MainPageHeader extends Component {
                 </ul>
               )
             }
-            <div className="first-section-m__image">
+            <div className={css(styles.firstSectionMImage)}>
               <picture>
                 <img
                   src={require('./images/first-section-m-img.png')}
@@ -112,6 +114,40 @@ class MainPageHeader extends Component {
     );
   }
 }
+
+const styles = StyleSheet.create({
+  firstSectionM: {
+    backgroundColor: '#F2F1EB',
+    width: '100%',
+    minHeight: '640px',
+    maxWidth: '100%',
+    position: 'relative',
+    fontFamily: 'GilroyRegular',
+    paddingBottom: '64px',
+  },
+  firstSectionMData: {
+    marginTop: '98px',
+    '@media screen and (min-width: 720px)': { textAlign: 'center' },
+  },
+  firstSectionMHeader: {
+    fontFamily: 'GilroyBold',
+    fontSize: '36px',
+    lineHeight: '44px',
+    marginBottom: '24px',
+    marginTop: 0,
+  },
+  firstSectionMDescr: { marginBottom: '28px' },
+  firstSectionMContainer: {
+    paddingLeft: '24px',
+    paddingRight: '24px',
+    paddingTop: '76px',
+  },
+  firstSectionMImage: {
+    display: 'flex',
+    justifyContent: 'center',
+    marginTop: '24px',
+  },
+});
 
 MainPageHeader.propTypes = {
   user: PropTypes.object,
