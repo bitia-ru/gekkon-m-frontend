1bf1b7c5f43609de4c37ff542ea8b9175f8bab4a
Svetlana Batova <sbatova@bitia.ru>
2020-09-04 11:51:49 +0300

    GFM-82: Change Logo.css to aphrodite

--- src/v2/components/Logo/Logo.js
+++ src/v2/components/Logo/Logo.js
@@ -1,10 +1,10 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
-import './Logo.css';
+import { StyleSheet, css } from '../../aphrodite';
 
 const Logo = () => (
-  <Link to="/" className="logo-m">
-    <span className="logo-m__icon">
+  <Link to="/" className={css(styles.logoM)}>
+    <span className={css(styles.logoMIcon)}>
       <img
         src={require('./images/logo-75x75.png')}
         width="75px"
@@ -14,5 +14,34 @@ const Logo = () => (
     </span>
   </Link>
 );
+
+const styles = StyleSheet.create({
+  logoM: {
+    width: '76px',
+    height: '76px',
+    position: 'fixed',
+    left: 0,
+    top: 0,
+    backgroundColor: '#0D0D0D',
+    display: 'flex',
+    alignItems: 'center',
+    justifyContent: 'center',
+    transition: 'box-shadow .4s ease-out',
+    zIndex: 10,
+    ':focus': { boxShadow: '0px 0px 0px 2px rgba(0, 108, 235, 0.7)' },
+  },
+  logoMIcon: {
+    display: 'flex',
+    alignItems: 'center',
+    justifyContent: 'center',
+    width: '100%',
+    height: '100%',
+    boxSizing: 'border-box',
+    '> img': {
+      width: '100%',
+      height: '100%',
+    }
+  },
+});
 
 export default Logo;
