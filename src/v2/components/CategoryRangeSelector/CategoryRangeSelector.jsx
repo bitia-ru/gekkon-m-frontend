8b4330fdf8aebb0a09382901e90958d4aa042ec5
Svetlana Batova <sbatova@bitia.ru>
2020-09-04 15:31:50 +0300

    GFM-82: Change RouteDataEditableTable.css to aphrodite

--- src/v2/components/CategoryRangeSelector/CategoryRangeSelector.jsx
+++ src/v2/components/CategoryRangeSelector/CategoryRangeSelector.jsx
@@ -19,17 +19,17 @@ class CategoryRangeSelector extends React.PureComponent {
     const { categoryFrom, categoryTo, onChangeCategoryFilter } = this.props;
     return (
       <>
-        <div className="field-select-m__container">
+        <div className={css(styles.fieldSelectMContainer)}>
           <div
             role="button"
-            className="field-select-m__select field-select-m__select_active"
+            className={css(styles.fieldSelectMSelect)}
             onClick={() => this.setState({ expanded: true })}
             tabIndex="0"
             style={{ outline: 'none' }}
           >
-            <span className="field-select-m__placeholder">От</span>
+            <span className={css(styles.fieldSelectMPlaceholder)}>От</span>
             <Category category={categoryFrom} color={getCategoryColor(categoryFrom)} />
-            <span className="field-select-m__placeholder">до</span>
+            <span className={css(styles.fieldSelectMPlaceholder)}>до</span>
             <Category category={categoryTo} color={getCategoryColor(categoryTo)} />
           </div>
         </div>
@@ -50,8 +50,54 @@ class CategoryRangeSelector extends React.PureComponent {
   }
 }
 
-const style = StyleSheet.create({
-
+const styles = StyleSheet.create({
+  fieldSelectMContainer: { position: 'relative' },
+  fieldSelectMSelect: {
+    width: '100%',
+    height: '54px',
+    lineHeight: '1.3em',
+    display: 'flex',
+    alignItems: 'center',
+    border: '2px solid #DDE2EF',
+    borderRadius: 0,
+    padding: '14px 40px 14px 20px',
+    backgroundColor: '#ffffff',
+    outline: 'none',
+    transition: 'box-shadow .4s ease-out',
+    fontFamily: 'GilroyRegular',
+    fontSize: '14px',
+    boxSizing: 'border-box',
+    position: 'relative',
+    maxWidth: '100%',
+    overflow: 'hidden',
+    whiteSpace: 'nowrap',
+    ':before': {
+      content: '\'\'',
+      position: 'absolute',
+      right: '20px',
+      top: '50%',
+      width: '14px',
+      height: '9px',
+      transform: 'translateY(-50%)',
+      backgroundImage: 'url("data:image/svg+xml;charset=US-ASCII,%3Csvg%20width%3D%2214%22%20height%3D%229%22%20viewBox%3D%220%200%2014%209%22%20fill%3D%22none%22%20xmlns%3D%22http%3A//www.w3.org/2000/svg%22%3E%0A%3Cpath%20d%3D%22M7%209L0.0717972%20-1.30507e-06L13.9282%20-9.36995e-08L7%209Z%22%20fill%3D%22%231A1A1A%22/%3E%0A%3C/svg%3E%0A")',
+      backgroundRepeat: 'no-repeat',
+      backgroundSize: '100% auto',
+      pointerEvents: 'none',
+      zIndex: 2,
+    },
+    ':after': {
+      width: '45px',
+      height: '100%',
+      backgroundColor: '#ffffff',
+      position: 'absolute',
+      content: '\'\'',
+      right: 0,
+      top: 0,
+      zIndex: 1,
+    },
+    ':focus': { boxShadow: '0px 0px 0px 2px rgba(0, 108, 235, 0.7)' },
+  },
+  fieldSelectMPlaceholder: { marginRight: '10px' },
 });
 
 CategoryRangeSelector.propTypes = {
