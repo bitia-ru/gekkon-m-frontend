d5e5f301bb24bb1e206448a01297822871141038
Svetlana Batova <sbatova@bitia.ru>
2020-09-04 14:05:21 +0300

    GFM-82: Change Pagination.css to aphrodite

--- src/v2/components/Pagination/Pagination.js
+++ src/v2/components/Pagination/Pagination.js
@@ -1,59 +1,146 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import * as R from 'ramda';
-import './Pagination.css';
 import { StyleSheet, css } from '../../aphrodite';
 
 const Pagination = ({
   page, firstPage, lastPage, pagesList, onPageChange,
-}) => {
-  const pageActiveClass = currentPage => (
-    currentPage === page ? ' pagination-m__item_active' : ''
-  );
-  return (
-    <div className={css(styles.contentMPagination)}>
-      {
-        pagesList.length > 1 && (
-          <div className="pagination-m">
+}) => (
+  <div className={css(styles.contentMPagination)}>
+    {
+      pagesList.length > 1 && (
+        <div className={css(styles.paginationM)}>
+          <div
+            role="button"
+            tabIndex="0"
+            style={{ outline: 'none' }}
+            className={
+              css(
+                styles.paginationMItem,
+                styles.paginationMToggle,
+                styles.paginationMTogglePrev,
+              )
+            }
+            onClick={() => onPageChange(firstPage)}
+          />
+          {R.map(currentPage => (
             <div
+              key={currentPage}
               role="button"
               tabIndex="0"
               style={{ outline: 'none' }}
-              className="pagination-m__item pagination-m__toggle pagination-m__toggle-prev"
-              onClick={() => onPageChange(firstPage)}
-            />
-            {R.map(currentPage => (
-              <div
-                key={currentPage}
-                role="button"
-                tabIndex="0"
-                style={{ outline: 'none' }}
-                onClick={() => onPageChange(currentPage)}
-                className={`pagination-m__item${pageActiveClass(currentPage)}`}
-              >
-                {currentPage}
-              </div>
-            ), pagesList)}
-            <div
-              role="button"
-              tabIndex="0"
-              style={{ outline: 'none' }}
-              className="pagination-m__item pagination-m__toggle pagination-m__toggle-next"
-              onClick={() => onPageChange(lastPage)}
-            />
-          </div>
-        )
-      }
-    </div>
-  );
-};
+              onClick={() => onPageChange(currentPage)}
+              className={
+                css(
+                  styles.paginationMItem,
+                  currentPage === page && styles.paginationMItemActive,
+                )
+              }
+            >
+              {currentPage}
+            </div>
+          ), pagesList)}
+          <div
+            role="button"
+            tabIndex="0"
+            style={{ outline: 'none' }}
+            className={
+              css(
+                styles.paginationMItem,
+                styles.paginationMToggle,
+                styles.paginationMToggleNext,
+              )
+            }
+            onClick={() => onPageChange(lastPage)}
+          />
+        </div>
+      )
+    }
+  </div>
+);
 
 const styles = StyleSheet.create({
   contentMPagination: {
     marginTop: '30px',
     display: 'flex',
     justifyContent: 'center',
-  }
+  },
+  paginationM: { display: 'flex' },
+  paginationMItem: {
+    display: 'flex',
+    alignItems: 'center',
+    justifyContent: 'center',
+    textDecoration: 'none',
+    color: '#1A1A1A',
+    width: '36px',
+    height: '36px',
+    padding: '5px',
+    boxSizing: 'border-box',
+    transition: 'color, background-color .4s ease-out',
+    marginLeft: '4px',
+    marginRight: '4px',
+    flexShrink: 0,
+    ':hover': { backgroundColor: '#E6EBF2' },
+    ':focus': { backgroundColor: '#E6EBF2' },
+  },
+  paginationMItemActive: {
+    color: '#0871EC',
+  },
+  paginationMToggle: {
+    position: 'relative',
+    backgroundColor: '#ffffff',
+    boxShadow: '0px 2px 15px rgba(0, 0, 0, 0.04)',
+    ':before': {
+      position: 'absolute',
+      content: '\'\'',
+      backgroundColor: '#1f1f1f',
+      height: '1px',
+      width: '8px',
+    },
+    ':after': {
+      position: 'absolute',
+      content: '\'\'',
+      backgroundColor: '#1f1f1f',
+      height: '1px',
+      width: '8px',
+    },
+    ':hover': {
+      backgroundColor: '#E6EBF2',
+      ':before': { backgroundColor: '#006CEB' },
+      ':after': { backgroundColor: '#006CEB' },
+    },
+    ':focus': { backgroundColor: '#E6EBF2' },
+  },
+  paginationMToggleNext: {
+    marginLeft: 'auto',
+    ':before': {
+      top: '50%',
+      left: '50%',
+      transform: 'translate(-50%, -50%) rotate(45deg)',
+      transformOrigin: 'right bottom',
+    },
+    ':after': {
+      top: '50%',
+      left: '50%',
+      transform: 'translate(-50%, -50%) rotate(-45deg)',
+      transformOrigin: 'right top',
+    },
+  },
+  paginationMTogglePrev: {
+    marginRight: 'auto',
+    ':before': {
+      top: '50%',
+      left: '50%',
+      transform: 'translate(-50%, -50%) rotate(-45deg)',
+      transformOrigin: 'left bottom',
+    },
+    ':after': {
+      top: '50%',
+      left: '50%',
+      transform: 'translate(-50%, -50%) rotate(45deg)',
+      transformOrigin: 'left top',
+    },
+  },
 });
 
 Pagination.propTypes = {
