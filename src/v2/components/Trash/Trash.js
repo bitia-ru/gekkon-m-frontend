ef12d940d6140acae84ce6c47479d0679f7f0360
Svetlana Batova <sbatova@bitia.ru>
2020-09-08 10:19:02 +0300

    GFM-82: Switch Trash.js styles to aphrodite

--- src/v2/components/Trash/Trash.js
+++ src/v2/components/Trash/Trash.js
@@ -1,12 +1,10 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import './Trash.css';
+import { css, StyleSheet } from '@/v2/aphrodite';
 
 const Trash = ({
   setRef, visible, bottom, active,
 }) => {
-  const defaultClass = 'modal-block-m__trash-can modal-block-m__trash-can_active-';
-  const activeClass = active ? ' modal-block-m__trash-can_active-target' : '';
   return (
     <div
       style={
@@ -16,7 +14,11 @@ const Trash = ({
       }
       ref={setRef}
       className={
-        `${defaultClass}${bottom ? 'bottom' : 'top'}${activeClass}`
+        css(
+          styles.modalBlockMTrashCan,
+          bottom ? styles.modalBlockMTrashCanActiveBottom : styles.modalBlockMTrashCanActiveTop,
+          active && styles.modalBlockMTrashCanActiveTarget,
+        )
       }
     >
       <svg aria-hidden="true">
@@ -25,6 +27,41 @@ const Trash = ({
     </div>
   );
 };
+
+const styles = StyleSheet.create({
+  modalBlockMTrashCan: {
+    width: '100%',
+    display: 'flex',
+    justifyContent: 'center',
+    alignItems: 'center',
+    paddingTop: '18px',
+    paddingBottom: '18px',
+    backgroundColor: 'rgba(31, 31, 31, 0.7)',
+    transition: 'background-color .4s ease-out',
+    '> svg': {
+      width: '22px',
+      height: '24px',
+      fill: '#ffffff',
+    }
+  },
+  modalBlockMTrashCanActiveTop: {
+    position: 'fixed',
+    zIndex: '30',
+    content: '\'\'',
+    top: '0',
+    left: '0',
+  },
+  modalBlockMTrashCanActiveTarget: {
+    backgroundColor: 'rgba(167, 13, 13, 0.7)',
+  },
+  modalBlockMTrashCanActiveBottom: {
+    position: 'fixed',
+    zIndex: 30,
+    content: '\'\'',
+    bottom: 0,
+    left: 0,
+  },
+});
 
 Trash.propTypes = {
   bottom: PropTypes.bool,
