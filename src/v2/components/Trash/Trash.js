2be219d7e58b961b3408480d71964e38ece6c922
Svetlana Batova <sbatova@bitia.ru>
2020-09-08 09:48:10 +0300

    GFM-82: Move RouteEditor and Trash to v2

---
+++ src/v2/components/Trash/Trash.js
@@ -0,0 +1,42 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import './Trash.css';
+
+const Trash = ({
+  setRef, visible, bottom, active,
+}) => {
+  const defaultClass = 'modal-block-m__trash-can modal-block-m__trash-can_active-';
+  const activeClass = active ? ' modal-block-m__trash-can_active-target' : '';
+  return (
+    <div
+      style={
+        visible
+          ? {}
+          : { display: 'none' }
+      }
+      ref={setRef}
+      className={
+        `${defaultClass}${bottom ? 'bottom' : 'top'}${activeClass}`
+      }
+    >
+      <svg aria-hidden="true">
+        <use xlinkHref={`${require('./images/trash-can.svg')}#trash-can`} />
+      </svg>
+    </div>
+  );
+};
+
+Trash.propTypes = {
+  bottom: PropTypes.bool,
+  visible: PropTypes.bool,
+  active: PropTypes.bool,
+  setRef: PropTypes.func.isRequired,
+};
+
+Trash.defaultProps = {
+  visible: true,
+  bottom: false,
+  active: false,
+};
+
+export default Trash;
