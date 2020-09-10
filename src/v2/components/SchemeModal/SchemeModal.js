da6f0d8bf7758ff1fc351b752e2d68c95e9bbe23
Svetlana Batova <sbatova@bitia.ru>
2020-09-10 12:49:10 +0300

    GFM-82: Move SchemeModal to v2

---
+++ src/v2/components/SchemeModal/SchemeModal.js
@@ -0,0 +1,32 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import Scheme from '@/v1/components/Scheme/Scheme';
+import CloseButton from '@/v1/components/CloseButton/CloseButton';
+import './SchemeModal.css';
+
+const SchemeModal = ({ close, currentRoute }) => (
+  <div className="modal-block-m modal-block-m_dark">
+    <div className="modal-block-m__inner modal-block-m__inner_no-pb">
+      <div className="modal-block-m__container">
+        <div className="modal-block-m__header">
+          <div className="modal-block-m__header-btn">
+            <CloseButton onClick={close} light />
+          </div>
+        </div>
+      </div>
+      <div className="modal-block-m__map">
+        <Scheme
+          currentRoutes={[currentRoute]}
+          showCards={false}
+        />
+      </div>
+    </div>
+  </div>
+);
+
+SchemeModal.propTypes = {
+  currentRoute: PropTypes.object.isRequired,
+  close: PropTypes.func.isRequired,
+};
+
+export default SchemeModal;
