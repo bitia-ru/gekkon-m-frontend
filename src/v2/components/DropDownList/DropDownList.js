4e76e2cb51278f5a8ab1cd2f1aef0c0770dfb99d
Svetlana Batova <sbatova@bitia.ru>
2020-09-10 22:17:18 +0300

    GFM-82: Change DropDownList.css to aphrodite

--- src/v2/components/DropDownList/DropDownList.js
+++ src/v2/components/DropDownList/DropDownList.js
@@ -3,7 +3,7 @@ import PropTypes from 'prop-types';
 import * as R from 'ramda';
 import Button from '@/v1/components/Button/Button';
 import CloseButton from '@/v1/components/CloseButton/CloseButton';
-import './DropDownList.css';
+import { StyleSheet, css } from '@/v2/aphrodite';
 
 export default class DropDownList extends Component {
   constructor(props) {
@@ -62,13 +62,13 @@ export default class DropDownList extends Component {
                 <CloseButton onClick={hide} light />
               </div>
             </div>
-            <div className="dropdown-list">
-              <ul className="dropdown-list__inner">
+            <div className={css(styles.dropdownList)}>
+              <ul className={css(styles.dropdownListInner)}>
                 {
                   R.map(item => (
                     <li
                       key={item.id}
-                      className="dropdown-list__item"
+                      className={css(styles.dropdownListItem)}
                     >
                       <div
                         role="button"
@@ -89,7 +89,7 @@ export default class DropDownList extends Component {
               {
                 multipleSelect
                   ? (
-                    <div className="dropdown-list__buttons-wrapper">
+                    <div className={css(styles.dropdownListButtonsWrapper)}>
                       <Button
                         size="big"
                         buttonStyle="normal"
@@ -98,7 +98,7 @@ export default class DropDownList extends Component {
                         onClick={this.save}
                       />
                       <Button
-                        customClass="dropdown-list__button-cancel"
+                        customClass={css(styles.dropdownListButtonCancel)}
                         title="Сбросить"
                         onClick={this.setDefault}
                       />
@@ -113,6 +113,65 @@ export default class DropDownList extends Component {
     );
   }
 }
+
+const styles = StyleSheet.create({
+  dropdownList: {
+    backgroundColor: '#ffffff',
+    display: 'block',
+  },
+  dropdownListInner: {
+    margin: 0,
+    padding: 0,
+    backgroundColor: '#ffffff',
+    minWidth: '100%',
+    boxSizing: 'border-box',
+    display: 'block',
+    zIndex: 10,
+  },
+  dropdownListItem: {
+    listStyle: 'none',
+    lineHeight: '1.3em',
+    color: '#1f1f1f',
+    fontFamily: 'GilroyRegular',
+    fontSize: '16px',
+    padding: '16px 20px',
+    maxWidth: '100%',
+    overflowX: 'hidden',
+    whiteSpace: 'nowrap',
+    height: '54px',
+    boxSizing: 'border-box',
+    ':hover': { backgroundColor: '#f5f5f5' },
+  },
+  dropdownListButtonsWrapper: {
+    width: '100%',
+    boxSizing: 'border-box',
+    paddingLeft: '16px',
+    paddingRight: '16px',
+    paddingBottom: '10px',
+  },
+  dropdownListButtonCancel: {
+    padding: 0,
+    paddingTop: '14px',
+    paddingBottom: '14px',
+    backgroundColor: 'transparent',
+    border: 'none',
+    color: '#C2C3C8',
+    fontSize: '14px',
+    fontFamily: 'GilroyRegular, sans-serif',
+    width: '100%',
+    transition: '.4s ease-out',
+    position: 'relative',
+    marginTop: '10px',
+    ':hover': {
+      backgroundColor: '#C6C7CC',
+      color: '#1f1f1f',
+    },
+  },
+  dropdownListSaveBtn: {
+    paddingRight: '16px',
+    paddingLeft: '16px',
+  },
+});
 
 DropDownList.propTypes = {
   multipleSelect: PropTypes.bool,
