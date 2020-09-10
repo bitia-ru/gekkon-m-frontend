7807bd9e03a16952b53983d58984ca33138b139a
Svetlana Batova <sbatova@bitia.ru>
2020-09-10 18:19:29 +0300

    GFM-82: Change ButtonHandler.css to aphrodite

--- src/v2/components/ButtonHandler/ButtonHandler.js
+++ src/v2/components/ButtonHandler/ButtonHandler.js
@@ -1,16 +1,46 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import './ButtonHandler.css';
+import { StyleSheet, css } from '@/v2/aphrodite';
 
 const ButtonHandler = ({ title, xlinkHref, onClick }) => (
   <div className="route-m__route-footer-item">
-    <button type="button" className="btn-handler" title={title} onClick={onClick}>
+    <button type="button" className={css(styles.btnHandler)} title={title} onClick={onClick}>
       <svg aria-hidden="true">
         <use xlinkHref={xlinkHref} />
       </svg>
     </button>
   </div>
 );
+
+const styles = StyleSheet.create({
+  btnHandler: {
+    backgroundСolor: '#E4E4E4',
+    border: 'none',
+    boxShadow: 'none',
+    width: '36px',
+    height: '36px',
+    padding: '9px 9px',
+    cursor: 'pointer',
+    transition: 'background-color .4s ease-out',
+    display: 'flex',
+    alignItems: 'center',
+    justifyContent: 'center',
+    ':hover': {
+      backgroundColor: 'rgba(0, 108, 235, 0.7)',
+      '> svg': { fill: '#ffffff' },
+    },
+    ':focus': {
+      backgroundColor: 'rgba(0, 108, 235, 0.7)',
+      '> svg': { fill: '#ffffff' },
+    },
+    '> svg': {
+      width: '100%',
+      height: '100%',
+      fill: '#4F4F4F',
+      transition: 'fill .4s ease-out',
+    }
+  }
+});
 
 ButtonHandler.propTypes = {
   onClick: PropTypes.func.isRequired,
