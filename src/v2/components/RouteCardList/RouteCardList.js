083a21c29a9eac57191467e844314824de0dfbbd
Svetlana Batova <sbatova@bitia.ru>
2020-09-10 23:55:51 +0300

    GFM-94: Copy RouteCardList, RouteCardScheme, RouteCardTable, RouteCardView to v2

---
+++ src/v2/components/RouteCardList/RouteCardList.js
@@ -0,0 +1,75 @@
+import React from 'react';
+import { withRouter } from 'react-router-dom';
+import { connect } from 'react-redux';
+import PropTypes from 'prop-types';
+import * as R from 'ramda';
+import RouteRow from '@/v1/components/RouteRow/RouteRow';
+import getArrayByIds from '@/v1/utils/getArrayByIds';
+import SectorContext from '@/v1/contexts/SectorContext';
+import AddRouteButton from '@/v1/components/AddRouteButton/AddRouteButton';
+import './RouteCardList.css';
+
+const RouteCardList = ({
+  user,
+  addRoute,
+  onRouteClick,
+  routes,
+  routeIds,
+}) => (
+  <SectorContext.Consumer>
+    {
+      ({ sector }) => (
+        <>
+          <div className="content-m__inner-card">
+            {
+              (sector && user) && <AddRouteButton onClick={addRoute} />
+            }
+          </div>
+          <div className="content-m__inner-table">
+            <div className="content-m__col-sm-12">
+              <div className="table-m">
+                <div className="table-m__header">
+                  <div className="table-m__header-item table-m__header-item_number">№</div>
+                  <div className="table-m__header-item table-m__header-item_level">Категория</div>
+                  <div className="table-m__header-item table-m__header-item_hook">Зацепы</div>
+                </div>
+                {
+                  R.map(
+                    route => (
+                      <RouteRow
+                        key={route.id}
+                        route={route}
+                        onRouteClick={() => onRouteClick(route.id)}
+                      />
+                    ),
+                    getArrayByIds(routeIds, routes),
+                  )
+                }
+              </div>
+            </div>
+          </div>
+        </>
+      )
+    }
+  </SectorContext.Consumer>
+);
+
+RouteCardList.propTypes = {
+  user: PropTypes.object,
+  routes: PropTypes.object.isRequired,
+  routeIds: PropTypes.array.isRequired,
+  addRoute: PropTypes.func.isRequired,
+  onRouteClick: PropTypes.func.isRequired,
+};
+
+const mapStateToProps = state => ({
+  routes: state.routesStore.routes,
+  routeIds: (
+    state.routesStore.filtrationResults[0]
+      ? state.routesStore.filtrationResults[0].routeIds
+      : []
+  ),
+  user: state.usersStore.users[state.usersStore.currentUserId],
+});
+
+export default withRouter(connect(mapStateToProps)(RouteCardList));
