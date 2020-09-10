18bb0b34793eaab48c6dea411c3e2840aded24f3
Svetlana Batova <sbatova@bitia.ru>
2020-09-10 14:24:44 +0300

    GFM-82: Change RouteColorPicker.css to aphrodite(part 1)

--- src/v2/components/RouteColorPicker/RouteColorPicker.js
+++ src/v2/components/RouteColorPicker/RouteColorPicker.js
@@ -5,6 +5,7 @@ import CloseButton from '@/v1/components/CloseButton/CloseButton';
 import Button from '@/v1/components/Button/Button';
 import getColorStyle from '@/v1/Constants/RouteColorPicker';
 import './RouteColorPicker.css';
+import { StyleSheet, css } from '@/v2/aphrodite';
 
 export default class RouteColorPicker extends Component {
   selectItem = (routeMarkColor) => {
@@ -34,26 +35,33 @@ export default class RouteColorPicker extends Component {
                 <CloseButton onClick={hide} light />
               </div>
             </div>
-            <div className="route-color-picker">
-              <div className="route-color-picker__wrapper">
+            <div className={css(styles.routeColorPicker)}>
+              <div className={css(styles.routeColorPickerWrapper)}>
                 {
                   R.map(routeMarkColor => (
                     <li
                       key={routeMarkColor.id}
-                      className="route-color-picker__item route-color-picker__item_padding-10"
+                      className={
+                        css(
+                          styles.routeColorPickerItem,
+                          styles.routeColorPickerItemPadding10,
+                        )
+                      }
                     >
                       <div
                         role="button"
                         tabIndex="0"
                         style={{ outline: 'none' }}
-                        className="mark-color-picker__item"
+                        className={css(styles.markColorPickerItem)}
                         onClick={() => this.selectItem(routeMarkColor)}
                       >
                         <div
-                          className="mark-color-picker__color"
+                          className={css(styles.markColorPickerColor)}
                           style={getColorStyle(routeMarkColor)}
                         />
-                        <div className="mark-color-picker__item-text">{routeMarkColor.name}</div>
+                        <div className={css(styles.markColorPickerItemText)}>
+                          {routeMarkColor.name}
+                        </div>
                       </div>
                     </li>
                   ),
@@ -61,7 +69,7 @@ export default class RouteColorPicker extends Component {
                 }
               </div>
               <Button
-                customClass="route-color-picker__button-cancel"
+                customClass={css(styles.routeColorPickerButtonCancel)}
                 title="Сбросить"
                 onClick={this.removeRouteMarkColor}
               />
@@ -72,6 +80,82 @@ export default class RouteColorPicker extends Component {
     );
   }
 }
+
+const styles = StyleSheet.create({
+  markColorPickerColor: {
+    display: 'inline-block',
+    width: '60px',
+    height: '20px',
+    verticalAlign: 'middle',
+    marginLeft: '15px',
+  },
+  markColorPickerItem: {
+    display: 'flex',
+    alignItems: 'center',
+    overflow: 'hidden',
+  },
+  markColorPickerItemText: {
+    fontSize: '16px',
+    fontFamily: 'GilroyRegular, sans-serif',
+    paddingLeft: '20px',
+  },
+  routeColorPicker: {
+    backgroundColor: '#ffffff',
+    minWidth: '100%',
+    boxSizing: 'border-box',
+    display: 'block',
+  },
+  routeColorPickerItem: {
+    listStyle: 'none',
+    lineHeight: '1.3em',
+    color: '#1f1f1f',
+    fontFamily: 'GilroyRegular',
+    fontSize: '18px',
+    padding: '16px 20px',
+    maxWidth: '100%',
+    overflowX: 'hidden',
+    whiteSpace: 'nowrap',
+    ':hover': {
+      backgroundColor: '#f5f5f5',
+      cursor: 'pointer',
+    },
+  },
+  routeColorPickerItemPadding10: { padding: '10px' },
+  routeColorPickerWrapper: {
+    paddingTop: '10px',
+    maxHeight: '270px',
+    overflowY: 'auto',
+    overflowX: 'hidden',
+    position: 'relative',
+  },
+  routeColorPickerButtonCancel: {
+    padding: 0,
+    height: '44px',
+    paddingTop: '14px',
+    paddingBottom: '14px',
+    backgroundColor: 'transparent',
+    border: 'none',
+    color: '#C2C3C8',
+    fontSize: '14px',
+    fontFamily: 'GilroyRegular, sans-serif',
+    width: '100%',
+    transition: '.4s ease-out',
+    position: 'relative',
+    ':hover': {
+      backgroundColor: '#C6C7CC',
+      color: '#1f1f1f',
+    },
+    ':before': {
+      position: 'absolute',
+      content: '\'\'',
+      left: '10px',
+      right: '10px',
+      top: 0,
+      height: '1px',
+      backgroundColor: '#EBEBEB',
+    },
+  },
+});
 
 RouteColorPicker.propTypes = {
   hide: PropTypes.func.isRequired,
