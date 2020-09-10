6dbd6a0648c4fd5bebc7c6a390efcd6dd04c4172
Svetlana Batova <sbatova@bitia.ru>
2020-09-10 23:55:51 +0300

    GFM-94: Copy RouteCardList, RouteCardScheme, RouteCardTable, RouteCardView to v2

---
+++ src/v2/components/RouteCardView/RouteCardView.js
@@ -0,0 +1,54 @@
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+import RouteCardTable from '../RouteCardTable/RouteCardTable';
+import RouteCardList from '../RouteCardList/RouteCardList';
+import RouteCardScheme from '../RouteCardScheme/RouteCardScheme';
+
+export default class RouteCardView extends Component {
+  renderViewMode(viewMode) {
+    const {
+      addRoute, onRouteClick, diagram,
+    } = this.props;
+    switch (viewMode) {
+    case 'scheme':
+      return (
+        <RouteCardScheme
+          diagram={diagram}
+          onRouteClick={onRouteClick}
+        />
+      );
+    case 'table':
+      return (
+        <RouteCardTable
+          addRoute={addRoute}
+          onRouteClick={onRouteClick}
+        />
+      );
+    case 'list':
+      return (
+        <RouteCardList
+          addRoute={addRoute}
+          onRouteClick={onRouteClick}
+        />
+      );
+    default:
+      return '';
+    }
+  }
+
+  render() {
+    const { viewMode } = this.props;
+    return (
+      <React.Fragment>
+        {this.renderViewMode(viewMode)}
+      </React.Fragment>
+    );
+  }
+}
+
+RouteCardView.propTypes = {
+  diagram: PropTypes.string,
+  viewMode: PropTypes.string.isRequired,
+  addRoute: PropTypes.func.isRequired,
+  onRouteClick: PropTypes.func.isRequired,
+};
