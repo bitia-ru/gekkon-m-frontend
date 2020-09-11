e04442dc024c92899cf418b69c3e368d265005e7
Svetlana Batova <sbatova@bitia.ru>
2020-09-11 10:42:26 +0300

    GFM-82: Change Context.css to aphrodite

--- src/v2/components/AddRouteButton/AddRouteButton.js
+++ src/v2/components/AddRouteButton/AddRouteButton.js
@@ -1,9 +1,10 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import './AddRouteButton.css';
+import { StyleSheet, css } from '../../aphrodite';
 
 const AddRouteButton = ({ onClick }) => (
-  <div className="content-m__col-sm-6 content-m__col-xs-12">
+  <div className={css(styles.contentMColSm6, styles.contentMColXs12)}>
     <a
       role="link"
       tabIndex="0"
@@ -16,6 +17,21 @@ const AddRouteButton = ({ onClick }) => (
     </a>
   </div>
 );
+
+const styles = StyleSheet.create({
+  contentMColXs12: {
+    width: '100%',
+    maxWidth: '100%',
+  },
+  contentMColSm6: {
+    '@media screen and (mix-width: 720px)': {
+      width: 'calc(50% - 24px)',
+      maxWidth: '50%',
+      marginLeft: '12px',
+      marginRight: '12px',
+    },
+  },
+});
 
 AddRouteButton.propTypes = {
   onClick: PropTypes.func.isRequired,
--- src/v2/components/InfoPageContent/InfoPageContent.js
+++ src/v2/components/InfoPageContent/InfoPageContent.js
@@ -1,22 +1,22 @@
 import React from 'react';
 import * as R from 'ramda';
 import PropTypes from 'prop-types';
-import './InfoPageContent.css';
+import { StyleSheet, css } from '../../aphrodite';
 
 const InfoPageContent = ({ titles, data }) => {
   const mapIndexed = R.addIndex(R.map);
   return (
-    <div className="content-m content-m_white">
-      <div className="content-m__container">
+    <div className={css(styles.contentM, styles.contentMWhite)}>
+      <div className={css(styles.contentMContainer)}>
         {
           mapIndexed(
             (paragraphs, index) => (
               <React.Fragment key={index}>
-                <h1 className="content-m__header">{titles[index]}</h1>
+                <h1 className={css(styles.contentMHeader)}>{titles[index]}</h1>
                 {
                   mapIndexed(
                     (paragraph, i) => (
-                      <p key={i} className="content-m__text">
+                      <p key={i} className={css(styles.contentMText)}>
                         {paragraph}
                       </p>
                     ),
@@ -32,6 +32,38 @@ const InfoPageContent = ({ titles, data }) => {
     </div>
   );
 };
+
+const styles = StyleSheet.create({
+  contentM: {
+    paddingTop: '68px',
+    paddingBottom: '68px',
+    backgroundСolor: '#FAFAFA',
+  },
+  contentMContainer: {
+    maxWidth: '1600px',
+    paddingLeft: '24px',
+    paddingRight: '24px',
+    marginRight: 'auto',
+    marginLeft: 'auto',
+  },
+  contentMHeader: {
+    fontSize: '24px',
+    fontFamily: 'GilroyMedium, sans-serif',
+    lineHeight: '32px',
+    marginTop: 0,
+    marginBottom: '36px',
+  },
+  contentMText: {
+    fontSize: '16px',
+    fontFamily: 'GilroyRegular, sans-serif',
+    lineHeight: '24px',
+    color: '#3F3F3F',
+    marginBottom: '24px',
+  },
+  contentMWhite: {
+    backgroundColor: '#ffffff',
+  },
+});
 
 InfoPageContent.propTypes = {
   titles: PropTypes.array.isRequired,
