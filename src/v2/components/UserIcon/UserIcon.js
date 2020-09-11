dbbbc419e950327cd7036362a405a54a19a7ce55
Svetlana Batova <sbatova@bitia.ru>
2020-09-11 11:40:18 +0300

    GFM-82: Change MainMenu.css to aphrodite

--- src/v2/components/UserIcon/UserIcon.js
+++ src/v2/components/UserIcon/UserIcon.js
@@ -3,20 +3,32 @@ import PropTypes from 'prop-types';
 import Avatar from '@/v1/components/Avatar/Avatar';
 import { getUserName } from '@/v1/Constants/User';
 import './UserIcon.css';
+import { StyleSheet, css } from '../../aphrodite';
 
 const UserIcon = ({
   user, hideMenu,
 }) => (
-  <div className="m-menu__user-block">
+  <div className={css(styles.mMenuUserBlock)}>
     <Avatar user={user} />
-    <div className="m-menu__user-name">{user ? getUserName(user, true) : ''}</div>
-    <div className="m-menu__user-hamburger">
+    <div className={css(styles.mMenuUserName)}>{user ? getUserName(user, true) : ''}</div>
+    <div className={css(styles.mMenuUserHamburger)}>
       <button type="button" className="hamburger hamburger_close" onClick={hideMenu}>
         <span className="hamburger__inner" />
       </button>
     </div>
   </div>
 );
+
+const styles = StyleSheet.create({
+  mMenuUserBlock: {
+    height: '76px',
+    marginBottom: '20px',
+    display: 'flex',
+    alignItems: 'center',
+  },
+  mMenuUserName: { marginLeft: '20px' },
+  mMenuUserHamburger: { marginLeft: 'auto' },
+});
 
 UserIcon.propTypes = {
   user: PropTypes.object,
