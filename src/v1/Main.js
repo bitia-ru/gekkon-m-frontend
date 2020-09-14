309f2fe82cf1bcdd4b58e47a8d3d558594060093
Svetlana Batova <sbatova@bitia.ru>
2020-09-14 21:54:59 +0300

    GFM-101: Refactor About and Faq to use MainScreen and switch it to v2

--- src/v1/Main.js
+++ src/v1/Main.js
@@ -2,8 +2,6 @@ import React from 'react';
 import { connect } from 'react-redux';
 import { Switch, Route, withRouter } from 'react-router-dom';
 import CragsIndex from './components/Crags/CragsIndex';
-import About from './components/About/About';
-import Faq from './components/Faq/Faq';
 import V2 from '@/v2/V2';
 import BootingScreen from './components/BootingScreen/BootingScreen';
 
@@ -17,8 +15,6 @@ const Main = ({ currentUserId }) => (
         : (
           <Switch>
             <Route exact path="/crags" component={CragsIndex} />
-            <Route exact path="/about" component={About} />
-            <Route exact path="/faq" component={Faq} />
             <Route path={['/', '/spots']} component={V2} />
           </Switch>
         )
