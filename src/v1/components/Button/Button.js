b4def02ea16c1adf3e8f864373d73a7f02db86a4
ALev <alev@bitia.ru>
2020-09-16 02:54:17 +0300

    GFM-103: Add 'filter' style for Button component

--- src/v1/components/Button/Button.js
+++ src/v1/components/Button/Button.js
@@ -14,38 +14,42 @@ const Button = ({
   customClass,
   title,
 }) => {
-  let styleClass = '';
-  if (buttonStyle !== 'normal') {
-    if (buttonStyle === 'gray') {
-      styleClass = ' btn-m_gray';
-    } else {
-      styleClass = ' btn-m_transparent';
-    }
-  }
-  let sizeClass = '';
-  if (size === 'small') {
-    sizeClass = ' btn-m__small';
-  } else if (size === 'medium') {
-    sizeClass = ' btn-m__medium';
-  }
-  const fullLengthClass = fullLength ? ' btn-m_full-length' : '';
-  const submitClass = submit ? ' btn-m__submit' : '';
-  const bigFontClass = smallFont ? '' : ' first-section-m__btn';
+  const styleClass = {
+    gray: 'btn-m_gray',
+    filter: 'btn-m_filter',
+    normal: '',
+  }[buttonStyle] || 'btn-m_transparent';
+
+  const sizeClass = {
+    small: 'btn-m__small',
+    medium: 'btn-m__medium',
+  }[size] || '';
+
+  const fullLengthClass = fullLength ? 'btn-m_full-length' : '';
+  const submitClass = submit ? 'btn-m__submit' : '';
+  const bigFontClass = smallFont ? '' : 'first-section-m__btn';
   let cursorStyle = {};
   if (disabled) {
     cursorStyle = { cursor: 'not-allowed' };
   } else if (isWaiting) {
     cursorStyle = { cursor: 'wait' };
   }
+  const normalClassName = [
+    'btn-m',
+    styleClass,
+    sizeClass,
+    fullLengthClass,
+    submitClass,
+    bigFontClass,
+  ].join(' ');
+
   return (
     <button
       type="button"
       onClick={onClick}
       disabled={disabled}
       style={cursorStyle}
-      className={
-        customClass || `btn-m${styleClass}${sizeClass}${fullLengthClass}${submitClass}${bigFontClass}`
-      }
+      className={customClass || normalClassName}
     >
       {title}
     </button>
