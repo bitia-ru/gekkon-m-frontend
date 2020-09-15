0f4d2590bdeafe0381d3e426321db4b22d2762a1
Svetlana Batova <sbatova@bitia.ru>
2020-09-15 14:05:20 +0300

    GFM-104: Introduce theme

---
+++ src/v2/theme.js
@@ -0,0 +1,32 @@
+const theme = {
+  controls: {
+    buttons: {
+      notDefault: {
+        background: '#EFEFEF',
+        foreground: '#333333',
+        hovered: {
+          background: '#f2f2f2',
+          foreground: '#000000',
+        },
+        pressed: {
+          background: '#E8E8E8',
+          foreground: '#333333',
+        },
+      },
+      default: {
+        background: '#006CEB',
+        foreground: '#FFFFFF',
+        hovered: {
+          background: 'rgba(0,108,235,0.9)',
+          foreground: '#FFFFFF',
+        },
+        pressed: {
+          background: '#006CEB',
+          foreground: '#FFFFFF',
+        },
+      },
+    },
+  },
+};
+
+export default theme;
