2fe1c5cf3fda9df8e56cb7ed730b40adbf3c1c8e
Sergey <vertigomaill@gmail.com>
2020-09-25 09:39:15 +0300

    GFM-133: Fix the date display in DatePickerSelector

--- src/v1/Constants/Date.js
+++ src/v1/Constants/Date.js
@@ -4,9 +4,4 @@ export const COMMENT_DATETIME_FORMAT = 'hh:mm LL';
 export const DATE_FORMAT = 'DD.MM.YYYY';
 export const BACKEND_DATE_FORMAT = 'YYYY-MM-DD';
 
-export const dateToTextFormatter = (date) => {
-  if (moment(date).format(DATE_FORMAT) === moment().format(DATE_FORMAT)) {
-    return 'сегодня';
-  }
-  return moment(date).format(DATE_FORMAT);
-};
+export const dateToTextFormatter = date => moment(date).format(DATE_FORMAT);
