cdb068bbd61995bde5d770cacf77ca81a24e5174
Svetlana Batova <sbatova@bitia.ru>
2020-11-30 16:35:46 +0300

    GFM-173: Add to period two weeks filter, year -> two month

--- src/v1/Constants/PeriodFilters.js
+++ src/v1/Constants/PeriodFilters.js
@@ -2,8 +2,9 @@ const PERIOD_FILTERS = [
   { id: 0, text: 'Все время', clickable: true },
   { id: 1, text: 'День', clickable: true },
   { id: 2, text: 'Неделя', clickable: true },
-  { id: 3, text: 'Месяц', clickable: true },
-  { id: 4, text: 'Год', clickable: true },
+  { id: 3, text: 'Две недели', clickable: true },
+  { id: 4, text: 'Месяц', clickable: true },
+  { id: 5, text: 'Два месяца', clickable: true },
 ];
 
 export { PERIOD_FILTERS as default };
