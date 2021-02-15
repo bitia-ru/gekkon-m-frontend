6b32188a7aaf15d12d57477d4522b0a4267a8f2d
ALev <alev@bitia.ru>
2021-02-15 03:25:38 +0300

    GFM-188: Fix ErrorBoundary and enhance its page

--- src/ErrorBoundary.jsx
+++ src/ErrorBoundary.jsx
@@ -14,9 +14,19 @@ class ErrorBoundary extends React.Component {
     Sentry.captureException(error);
     console.log(info);
 
-    if (localForage.length() > 0) {
-      await localForage.clear();
+    const reduxState = await localForage.getItem('reduxState');
 
+    if (!reduxState) {
+      console.log('Redux store is not initialized.');
+      return;
+    }
+
+    const { initializedAt } = reduxState;
+
+    if (initializedAt !== undefined && new Date().getTime() - initializedAt > 3000) {
+      console.log('Attempting to reset redux store...');
+      await localForage.clear();
+      console.log('Cleared.');
       window.location.reload();
     }
   }
@@ -24,7 +34,19 @@ class ErrorBoundary extends React.Component {
   render() {
     if (this.state.hasError) {
       return <div className={css(styles.container)}>
-        Something went wrong...
+        <div className={css(styles.messageContainer)}>
+          <p>Что-то пошло не так, попробуйте перезагрузить страницу.</p>
+          <p>
+            Если проблема не пропадает, обратитесь с вопросом в
+            {' '}
+            <a href="https://vk.com/roving.climbers">группу Вконтакте</a>
+            {' '}
+            или
+            {' '}
+            <a href="https://t.me/RC_crags_discussions">телеграм-чат</a>
+            .
+          </p>
+        </div>
       </div>;
     }
     return this.props.children;
@@ -36,9 +58,20 @@ const styles = StyleSheet.create({
     width: '100vw',
     height: '100vh',
     backgroundColor: 'black',
-    lineHeight: '100vh',
-    textAlign: 'center',
     color: 'white',
+    display: 'flex',
+    flexFlow: 'column',
+    alignItems: 'center',
+    justifyContent: 'space-around',
+    fontFamily: 'GilroyRegular',
+    fontSize: '0.7em',
+  },
+  messageContainer: {
+    textAlign: 'center',
+    '> a': {
+      color: 'white',
+      fontFamily: 'GilroyBold',
+    },
   },
 });
 
