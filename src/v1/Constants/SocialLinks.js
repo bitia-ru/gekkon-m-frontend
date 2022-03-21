23b7f14d0da3f35381ab026c99f7c63a48882f93
ALev <alev@bitia.ru>
2022-03-21 18:20:07 +0300

    release/0.17.0: Remove Facebook and Instagram links

--- src/v1/Constants/SocialLinks.js
+++ src/v1/Constants/SocialLinks.js
@@ -1,3 +1,2 @@
 export const TELEGRAM_LINK = 'https://t.me/crags_ru';
-export const INSTA_LINK = 'https://www.instagram.com/rovingclimbers/';
 export const VK_LINK = 'https://vk.com/roving.climbers';
